6db70eddcb6d5c42a306a6cbadd292d61a0a20e5
loll947 <gharbig122@gmail.com>
2026-09-19 15:31:18 +0200

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// Mercury Vault
-// Main website scripts
-
-const discordTicket =
-    "https://discord.com/channels/1548817819200786443/1548824734744191036";
-
-
 document.addEventListener("DOMContentLoaded", () => {
-
-    console.log("Mercury Vault loaded.");
-
-
-    // Make sure all purchase buttons use the Discord ticket
-
-    document
-        .querySelectorAll(".buy-btn, .package-btn")
-        .forEach((button) => {
-
-            button.addEventListener("click", () => {
-
-                button.href = discordTicket;
-
-            });
-
-        });
-
+    console.log("Mercury Vault loaded successfully.");
 });
